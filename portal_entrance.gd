a5531ea53159f9d1d65da8c33b23ac278f2880c4
Joseph Sharratt <joseph.sharratt@gmail.com>
2025-01-04 14:07:52 +0000

    Rename some variables for clarity

--- portal_entrance.gd
+++ portal_entrance.gd
@@ -1,10 +1,10 @@
 extends Area3D
 
-@export var portal_number: int
+@export var user_defined_exit_portal: int
 
 func _ready():
-	# If portal_number wasn't set in editor, extract it from the node name
-	var number = portal_number if portal_number > 0 else str(name)[-1].to_int()
-	
+	# If user_defined_exit_portal wasn't set in editor, extract it from the node name
+	var exit_portal_number = user_defined_exit_portal if user_defined_exit_portal > 0 else str(name)[-1].to_int()
+
 	# Connect to the root node's portal function
-	body_entered.connect(get_parent()._on_portal_entrance_area_3d_body_entered.bind(number))
+	body_entered.connect(get_parent()._on_portal_entrance_area_3d_body_entered.bind(exit_portal_number))
