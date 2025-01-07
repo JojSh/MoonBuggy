b6890f09bb130159f68e77d6103ca19049c42365
Joseph Sharratt <joseph.sharratt@gmail.com>
2025-01-07 15:36:23 +0000

    Reorganise the flat level area so its structure matches the spherical worlds.

--- LevelSphereParent.gd
+++ LevelSphereParent.gd
@@ -2,6 +2,5 @@ extends StaticBody3D
 
 func _on_area_3d_body_entered(body):
 	if body is VehicleBody3D:
-		print("Updating vehicle's new center of gravity to: ", $GravityArea3D.global_position)
 		body.update_new_center_of_gravity_point($GravityArea3D.global_position)
-
+	
