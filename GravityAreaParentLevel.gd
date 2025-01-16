f7bc3bb4d96b8e1f4abd54959bf5e45e4d28bea1
Joseph Sharratt <joseph.sharratt@gmail.com>
2025-01-16 14:46:00 +0000

    Missing line from gravity replace fix

--- GravityAreaParentLevel.gd
+++ GravityAreaParentLevel.gd
@@ -3,6 +3,7 @@ extends StaticBody3D
 func _on_area_3d_body_entered(body):
 	if body is VehicleBody3D:
 		# Temporarily increase priority
+		$GravityArea3D.priority = 2
 		print("Priority increased to 2 on: ", name)
 		
 		# Create timer to reset priority
