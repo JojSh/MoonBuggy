9d037acba4fa1ce68aecdf0c5189515ce7b602d7
Joj Martin <>
2025-02-04 15:22:29 +0000

    Improve dynamic player count management

--- RootNode.gd
+++ RootNode.gd
@@ -4,7 +4,7 @@ var list_of_players = []
 @export var desired_number_players: int = 1
 
 func _ready():
-	add_players_to_list()
+	register_active_players()
 	setup_split_screen()
 
 	for player in list_of_players:
@@ -14,52 +14,49 @@ func _ready():
 func setup_split_screen():
 	if desired_number_players == 1:
 		# Single player mode - cleanup split screen containers
-		$SplitScreenGridContainer2P.queue_free()
-		$SplitScreenGridContainer3P.queue_free()
-		$SplitScreenGridContainer4P.queue_free()
+		for split_screen in $PlayerScreenManager/SplitScreens.get_children():
+			split_screen.queue_free()
+
 		$AudioListener3DBetweenPlayers.queue_free()
-		
-		# Remove all players except the first one
-		var players = $PlayerContainer.get_children()
-		for i in range(1, players.size()):
-			players[i].queue_free()
-		
 		return
 		
 	# Get the appropriate grid container for player count
-	var grid_container = get_node("SplitScreenGridContainer" + str(desired_number_players) + "P")
+	var grid_container = $PlayerScreenManager/SplitScreens.get_node("GridContainer" + str(desired_number_players) + "P")
 	
 	# Clean up unused grid containers
-	for i in range(2, 5):  # handles 2P, 3P, and 4P containers
-		if i != desired_number_players:
-			get_node("SplitScreenGridContainer" + str(i) + "P").queue_free()
+	var all_containers = $PlayerScreenManager/SplitScreens.get_children()
+	var unused_containers = all_containers.filter(func(container): 
+		# Extract the number from the container name (e.g., "GridContainer2P" -> 2)
+		var player_count = container.name.replace("GridContainer", "").replace("P", "").to_int()
+		return player_count != desired_number_players
+	)
+	for container in unused_containers: container.queue_free()
 	
 	# Free the single player camera
 	$Camera3D.queue_free()
 	
 	# Move players to their respective viewports
-	var players = $PlayerContainer.get_children()
 	var viewports = grid_container.get_children()
 	
-	for i in range(min(players.size(), desired_number_players)):
-		var player = players[i]
+	for i in desired_number_players:
 		var viewport_container = viewports[i]
+		var player = list_of_players[i]
 		var viewport = viewport_container.get_node("SubViewport")
 		
 		# Remove player from original parent
-		$PlayerContainer.remove_child(player)
+		$PlayerScreenManager/PlayerContainer.remove_child(player)
 		# Add to new viewport
 		viewport.add_child(player)
 
-func add_players_to_list():
-	# Get all players from PlayerContainer before they're moved
-	for player in $PlayerContainer.get_children():
-		list_of_players.append(player)
-
-func get_players_from_sub_viewport_container (sub_viewport_container):
-	var sub_viewport = sub_viewport_container.get_node("SubViewport")
-	var player = sub_viewport.get_node("PlayerBuggy")
-	list_of_players.append(player)
+func register_active_players():
+	# Get all players from PlayerContainer and manage them based on desired player count
+	var index = 0
+	for player in $PlayerScreenManager/PlayerContainer.get_children():
+		if index < desired_number_players:
+			list_of_players.append(player)
+		else:
+			player.queue_free()
+		index += 1
 
 func _on_portal_entrance_area_3d_body_entered(body, portal_number: int):
 	if !(body is VehicleBody3D):
